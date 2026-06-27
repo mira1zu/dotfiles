55be77788404a944ba03018c6f202b095f70a344
Bohdan Poshtarenko <mira1zu@mira1zu.com>
2026-06-27 08:36:50 -0700

    Update .emacs.d/init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,7 @@
      "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7"
      default))
  '(fringe-mode 0 nil (fringe))
- '(package-selected-packages '(gruvbox-theme)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,8 +24,28 @@
 (setq select-enable-clipboard t)
 (setq select-enable-primary t)
 
+;; UI
+(setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(setq visible-bell nil) ; MUST be nil, not t, because of that damn exclamation mark.
+(global-display-line-numbers-mode t)
+(column-number-mode t)
+(global-hl-line-mode t)
+
+;; Sane defaults
+(setq-default indent-tabs-mode nil)    ; spaces, not tabs
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq backup-by-copying t)
+(electric-pair-mode t)                 ; auto-close () [] {} ""
+(recentf-mode t)                       ; track recent files
+
+;; Window management
+(use-package ace-window
+  :ensure t
+  :bind ("M-o" . ace-window))
+
+(global-set-key (kbd "s-<return>") #'toggle-frame-maximized)
 
 (set-face-attribute 'default nil
 		    :font "SF Mono-14"
@@ -34,3 +54,5 @@
 (global-set-key (kbd "C-c l") #'org-store-link)
 (global-set-key (kbd "C-c a") #'org-agenda)
 (global-set-key (kbd "C-c c") #'org-capture)
+
+(which-key-mode)
